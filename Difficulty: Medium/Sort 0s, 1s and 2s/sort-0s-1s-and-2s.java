4032c3717ed7881ed1933d312b46bd15ee137ee7
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-08-03 20:00:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void sort012(int[] arr) {
+        int low = 0;
+        int mid = 0;
+        int high = arr.length - 1;
+
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                // Swap arr[low] and arr[mid]
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else { // arr[mid] == 2
+                // Swap arr[mid] and arr[high]
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+        }
+    }
+}
