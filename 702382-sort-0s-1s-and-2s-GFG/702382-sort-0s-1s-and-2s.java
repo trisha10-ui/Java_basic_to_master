c363b6d23a3be7282f9ad3a49ab728457f25c8c0
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-08-03 20:01:59 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 702382-sort-0s-1s-and-2s-GFG/702382-sort-0s-1s-and-2s.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void sort012(int[] arr) {
+        int low = 0;
+        int mid = 0;
+        int high = arr.length - 1;
+
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                // Swap arr[low] and arr[mid]
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else { // arr[mid] == 2
+                // Swap arr[mid] and arr[high]
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
