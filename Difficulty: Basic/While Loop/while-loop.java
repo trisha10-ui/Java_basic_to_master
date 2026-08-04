b19789f2de39b43f83b7185031b6a323386f9958
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-08-04 20:31:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/While Loop/while-loop.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static void utility(int x) {
+        // code here
+        
+        while(x>=0){
+            System.out.print(x+" ");
+            x--;
+        }
+        
+    }
+}
