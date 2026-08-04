4cd79e19dc40c42d1cafaa7de692b54b28a8e90d
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-08-04 20:31:29 +0530

    Added solution - Time: O(x), Space: O(1)

---
+++ 704474-while-loop-GFG/704474-while-loop.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static void utility(int x) {
+        // code here
+        
+        while(x>=0){
+            System.out.print(x+" ");
+            x--;
+        }
+        
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
