b29bacfc0e46156c6c9d8cc469547ac95936da1e
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-08-06 19:51:31 +0530

    Time: 29 ms (93.69%), Space: 59.1 MB (75.02%) - LeetHub - Time: O(n^2), Space: O(1)

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Early exit if the smallest number is greater than 0
+            if (nums[i] > 0) break;
+
+            // Skip duplicate values for the first element
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+
+                    // Skip duplicate values for the second element
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                    // Skip duplicate values for the third element
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
