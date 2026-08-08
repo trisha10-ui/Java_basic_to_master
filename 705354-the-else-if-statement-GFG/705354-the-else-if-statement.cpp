469811caed99031da990ddf48079e0da7aa07f18
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-08-08 21:48:35 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 705354-the-else-if-statement-GFG/705354-the-else-if-statement.cpp
@@ -0,0 +1,19 @@
+void utility(int number) {
+    // Write the if, else if, else statements below
+    if(number>100)
+    {
+        cout<<"Big"<<endl;
+    }
+    else if(number<10)
+    {
+        cout<<"Small"<<endl;
+    }
+    else
+    {
+        cout<<"Number"<<endl;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
